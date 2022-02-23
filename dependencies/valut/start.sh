9bb266f58c7783fe1102c3185073c1e9e08f1804
dhwang <dhwang@thoughtworks.com>
2022-02-23 13:43:24 +0800

    init commit.

--- dependencies/valut/start.sh
+++ dependencies/valut/start.sh
@@ -7,10 +7,10 @@ cp ../example1/dependencies/valut/valut.js ./functions/helloworld/
 cp ../example1/dependencies/valut/vaultClient.js ./functions/helloworld/
 
 echo "sed dependency"
-sed -i '.bak' 's/@google-cloud\/secret-manager/.\/vaultClient/g'  index.js
+sed -i 's/@google-cloud\/secret-manager/.\/vaultClient/g'  index.js
 
 echo "add new package.json"
-sed -i '.bak' 's/^    "@google-cloud\/secret-manager": "^3.1.0"/    "node-vault": "^0.9.22"/g' package.json
+sed -i 's/^    "@google-cloud\/secret-manager": "^3.1.0"/    "node-vault": "^0.9.22"/g' package.json
 
 
 
